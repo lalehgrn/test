17838ed5e8cd73fc653c3b0457cba2d6a4a14b4f
laleh <laleh@192.168.1.54>
2018-10-11 22:11:54 +0200

    Helper calculator and step 2_4

--- LalehTestBotify/src/botify/Helper.java
+++ LalehTestBotify/src/botify/Helper.java
@@ -27,5 +27,23 @@ public class Helper {
 		    }
 		
 	}	
+	
+	public static void Calculette (int num1, int num2, String operator) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\laleh\\Documents\\geckodriver\\geckodriver.exe");
+	      
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.id("lst-ib")).sendKeys(num1+ operator + num2);
+		driver.findElement(By.name("btnK")).click();
+		
+		//find all elements
+		WebElement result = driver.findElement(By.id("cwos")); 
+		int resultint = Integer.parseInt(result.getText());
+		Assertions.assertTrue(resultint==num1+num2);
+		driver.close();
+			
+	}
 }
+
 
