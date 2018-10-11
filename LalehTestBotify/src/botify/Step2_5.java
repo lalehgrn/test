c8cdd3080c99e1f21ce85abde6860436f787d307
laleh <laleh@192.168.1.54>
2018-10-11 22:19:09 +0200

    Step2_5

---
+++ LalehTestBotify/src/botify/Step2_5.java
@@ -0,0 +1,10 @@
+package botify;
+
+public class Step2_5 {
+	public static void main(String[] args) {
+		int randomNum1 = (int)(Math.random() * 100000);
+		int randomNum2 = (int)(Math.random() * 100000);
+		Helper.Calculette(randomNum1, randomNum2, "-");
+	}
+	
+}
